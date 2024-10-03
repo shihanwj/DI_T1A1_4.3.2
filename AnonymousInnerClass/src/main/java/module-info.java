443eb13db6dcb28d8eeff55870c8526cc1de0f38
shihanwj <shihanwei495@gmail.com>
2024-10-03 13:45:53 +0200

    Delete AnonymousInnerClass directory

--- AnonymousInnerClass/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module es.ieslosmontecillos.anonymousinnerclass {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.desktop;
-
-
-    opens es.ieslosmontecillos.anonymousinnerclass to javafx.fxml;
-    exports es.ieslosmontecillos.anonymousinnerclass;
-}
